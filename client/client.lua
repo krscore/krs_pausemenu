b5fbe99607720863c55b6463e59dd2318c757e84
Karos. <131356071+Krs-Scripts@users.noreply.github.com>
2024-11-01 12:30:00 +0100

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,5 +1,7 @@
+local pauseMenuActive = false
 
 local function openPauseMenu()
+    TriggerEvent('ox_inventory:closeInventory')
     local data = lib.callback.await('krs_pausemenu:getPlayerData', 100)
     if data then
         print('Received data: ', json.encode(data))
@@ -14,20 +16,25 @@ local function openPauseMenu()
         })
         SetNuiFocus(true, true)
         TriggerScreenblurFadeIn(0)
+        pauseMenuActive = true
     end
 end
 
 RegisterNUICallback('close', function(data)
     TriggerScreenblurFadeOut(0)
     SetNuiFocus(false, false)
+    pauseMenuActive = false
 end)
 
 local function updatePauseMenu()
     while true do
         Wait(0)
-        DisableControlAction(1, 200, true) 
-        if IsPauseMenuActive() then
-            TriggerScreenblurFadeOut(0)
+        if pauseMenuActive then
+            DisableControlAction(1, 200, true) 
+            if IsPauseMenuActive() then
+                TriggerScreenblurFadeOut(0)
+                pauseMenuActive = false
+            end
         end
     end
 end
@@ -38,7 +45,11 @@ lib.addKeybind({
     name = 'PauseMenu',
     description = 'Toggle Pause Menu',
     defaultKey = 'ESCAPE',
-    onPressed = openPauseMenu
+    onPressed = function()
+        if not pauseMenuActive then
+            openPauseMenu()
+        end
+    end
 })
 
 RegisterNUICallback('map', function(data, cb)
@@ -47,18 +58,21 @@ RegisterNUICallback('map', function(data, cb)
     PauseMenuceptionGoDeeper(0)
     PauseMenuceptionTheKick()
     SetNuiFocus(false, false)
+    pauseMenuActive = false
     cb('ok')
 end)
 
 RegisterNUICallback('settings', function(data, cb)
     ActivateFrontendMenu(GetHashKey('FE_MENU_VERSION_LANDING_MENU'), 0, -1)
     SetNuiFocus(false, false)
+    pauseMenuActive = false
     cb('ok')
 end)
 
 RegisterNUICallback('logout', function(data, cb)
     TriggerServerEvent("krs_pausemenu:exitGame")
     SetNuiFocus(false, false)
+    pauseMenuActive = false
     cb('ok')
 end)
 
@@ -66,5 +80,6 @@ RegisterNUICallback('relog', function(data, cb)
     ExecuteCommand('relog')
     TriggerScreenblurFadeOut(0)
     SetNuiFocus(false, false)
+    pauseMenuActive = false
     cb('ok')
 end)
